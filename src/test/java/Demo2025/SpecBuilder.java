085b26ab6bf62b31c516bf2aa257a25ec641faa1
tumelotj <hazardtumelo@gmail.com>
2025-01-10 12:31:16 +0200

    10/01/25: SpecBuilder

--- src/test/java/Demo2025/SpecBuilder.java
+++ src/test/java/Demo2025/SpecBuilder.java
@@ -1,14 +1,20 @@
 package Demo2025;
 
+import files.Payload;
 import io.restassured.RestAssured;
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
-import files.*;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.Assert;
 
-public class Basics {
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SpecBuilder {
     public static void main(String[] args) {
 
         RestAssured.baseURI="https://rahulshettyacademy.com";
@@ -17,14 +23,24 @@ public class Basics {
         //When - Sends the request (http method and resource)
         //Then - Validate the response (AssertThat method)
 
+        /*** Request Spec Builder ***/
+        RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+                .addQueryParam("key","qaclick123")
+                .setContentType(ContentType.JSON).build();
+        RequestSpecification rs = given().spec(req);
 
-      String response=given().log().all().queryParam("key","qaclick123").queryParam("Content-Type","json").body(Payload.addPlace())
-                .when().post("/maps/api/place/add/json").
-                then().log().all().assertThat().statusCode(200).body("scope",equalTo("APP"))
-                .header("server","Apache/2.4.52 (Ubuntu)").extract().response().asString();
+        /*** Response Spec Builder ***/
+        ResponseSpecification resSpec= new ResponseSpecBuilder().expectStatusCode(200)
+                .expectHeader("server","Apache/2.4.52 (Ubuntu)").build();
+
+      Response response=rs.body(Payload.addPlace())
+              .when().post("/maps/api/place/add/json")
+              .then().log().all().spec(resSpec).extract().response();
+
+      String respo = response.asString();
 
         //Add place-> Update place with new address -> Get Place to validate if new address is present in response
-        JsonPath js = new JsonPath(response);
+        JsonPath js = new JsonPath(respo);
         String placeId = js.getString("place_id");
         System.out.println("Place ID: "+placeId);
 
